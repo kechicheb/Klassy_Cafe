32a678c5297d88567ba8c7d5eff1c2151930f2f3
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-08-29 13:56:57 +0200

    preload has been added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+// toggle button
 let nav = document.querySelector("header .container .nav");
-nav.onclick =  function () {
-    nav.classList.toggle("active");
+nav.onclick = function () {
+  nav.classList.toggle("active");
+};
+// loading
+let loading = document.querySelector(".loading");
+window.onload = function () {
+  loading.style.display = "none";
 };
