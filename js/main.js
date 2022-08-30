d071ba44c0a6eb6e0e9d5a1c1ffd4c0687df5c86
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-08-30 14:23:53 +0200

    soliving problems in Header

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 let header = document.querySelector("header");
 window.onscroll = function () {
   if (window.scrollY >= 600) {
-   header.classList.add("fixed");
+    header.classList.add("fixed");
   } else {
-   header.classList.remove("fixed");
+    header.classList.remove("fixed");
   }
 };
 
@@ -13,6 +13,17 @@ let nav = document.querySelector("header .container .nav");
 nav.onclick = function () {
   nav.classList.toggle("active");
 };
+
+//header display none when onclick any link
+let link = document.querySelectorAll("header .container > ul.links li a ");
+let ulLinks = document.querySelector("header .container > ul.links");
+link.forEach(function (e) {
+  e.onclick = function () {
+    ulLinks.style.display = "none";
+    nav.classList.remove("active");
+  };
+});
+
 // loading
 let loading = document.querySelector(".loading");
 window.onload = function () {
