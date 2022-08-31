34c6e9d169db4737f4279ea6cd7b887ecdab8f0e
kechicheb <93807375+kechicheb@users.noreply.github.com>
2022-08-31 17:17:56 +0200

    finished Contact Us

--- js/main.js
+++ js/main.js
@@ -14,18 +14,38 @@ nav.onclick = function () {
   nav.classList.toggle("active");
 };
 
-//header display none when onclick any link
+// header display none when onclick any link
 let link = document.querySelectorAll("header .container > ul.links li a ");
 let ulLinks = document.querySelector("header .container > ul.links");
-link.forEach(function (e) {
-  e.onclick = function () {
-    ulLinks.style.display = "none";
-    nav.classList.remove("active");
-  };
-});
+if (parseInt(window.screen.width) < 767) {
+  link.forEach(function (e) {
+    e.onclick = function () {
+      ulLinks.style.display = "none";
+      nav.classList.remove("active");
+    };
+  });
+}
+if(parseInt(window.screen.width) > 767) {
+  ulLinks.style.display = "flex";
+}
+window.onresize  =function () {
+  if (parseInt(window.screen.width) < 767) {
+    link.forEach(function (e) {
+      e.onclick = function () {
+        ulLinks.style.display = "none";
+        nav.classList.remove("active");
+      };
+    });
+  }
+  if(parseInt(window.screen.width) > 767) {
+    ulLinks.style.display = "flex";
+  }
+
+};
 
 // loading
 let loading = document.querySelector(".loading");
 window.onload = function () {
   loading.style.display = "none";
 };
+console.log(window.screen.width);
